60186c70a246ec3e8b5e7ea8b7f740b1cfa69d57
Guilherme Alves <124222533+guilhermeprogramador@users.noreply.github.com>
2025-02-18 09:31:24 -0300

    Add files via upload
    Exercicios aula 3, revisar e melhorar e tentar fazer em casa de outras forma. Adicionar os exercicios no anki para treino e o codigo mais perfeito possivel como a carta back

---
+++ imc.py
@@ -0,0 +1,6 @@
+peso = float(input("peso"))
+altura = float(input("altura"))
+
+imc = peso / altura ** 2
+
+print (f"seu imc é {imc}")
---
+++ juroscompostos.py
@@ -0,0 +1,19 @@
+'''
+programa que calcula o investimento 
+exercicio 2, aula 3
+
+'''
+
+montante_Inicial = float(input("investimento"))
+
+taxa_Juros = float(input("Juros"))
+
+meses_Investimento = int(input("Meses investido"))
+
+montante_final = montante_Inicial * (1 + taxa_Juros) ** meses_Investimento
+
+print(montante_final)
+
+
+
+
