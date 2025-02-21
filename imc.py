fe6d23d28cc45fe0f4218906ee70b7888cebe4bb
Guilherme Alves <124222533+guilhermeprogramador@users.noreply.github.com>
2025-02-21 11:49:22 -0300

    Add files via upload

--- imc.py
+++ imc.py
@@ -1,6 +1,15 @@
+'''
+exercio 1, aula 3
+
+Calculadora de imc 
+
+'''
+
+
 peso = float(input("peso"))
 altura = float(input("altura"))
 
 imc = peso / altura ** 2
 
 print (f"seu imc é {imc}")
+
--- juroscompostos.py
+++ juroscompostos.py
@@ -10,7 +10,7 @@ taxa_Juros = float(input("Juros"))
 
 meses_Investimento = int(input("Meses investido"))
 
-montante_final = montante_Inicial * (1 + taxa_Juros) ** meses_Investimento
+montante_final = montante_Inicial * (1 + taxa_Juros) ** meses_Investimento 
 
 print(montante_final)
 
