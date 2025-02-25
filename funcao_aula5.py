68f06445d7df190c8ceea2e108263988dd1587ff
Guilherme Alves <124222533+guilhermeprogramador@users.noreply.github.com>
2025-02-25 09:32:50 -0300

    Create funcao_aula5.py

---
+++ funcao_aula5.py
@@ -0,0 +1,12 @@
+def conta(a,b,c):
+    return  a + b >= c and a + c >= b or c + b >= a 
+
+def operacao(conta):
+    
+    return 
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(operacao(a,b,c))
