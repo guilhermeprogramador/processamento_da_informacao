af18659b2162aebcdc97b2222e5da4be387d398e
Guilherme Alves <124222533+guilhermeprogramador@users.noreply.github.com>
2025-03-28 12:04:27 -0300

    Create codigo_megasena.py

---
+++ codigo_megasena.py
@@ -0,0 +1,7 @@
+for n1 in range(1, 56): #AQUI VAMOS ATÉ O 55
+    print(n1)
+    for n2 in range(1, 57): #AQUI VAMOS ATÉ O 56
+        for n3 in range( 1, 58): #AQUI VAMOS ATÉ O 57 
+            for n4 in range(1, 59): #AQUI VAMOS ATÉ O 58
+                for n5 in range(1, 60):
+                    for n6 in range(1, 61):
